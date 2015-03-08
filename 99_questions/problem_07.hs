7123d96433e03753e71bfb4e7f503f94ff80e81a
Mark deVilliers <markdevilliers@gmail.com>
2015-03-08 20:04:52 +0000

    adding in solution to problem 7

---
+++ 99_questions/problem_07.hs
@@ -0,0 +1,8 @@
+ --Flatten a nested list structure.
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem a)      = [a]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List [])     = []
